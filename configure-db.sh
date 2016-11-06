8e41979cba433dd1530a12252b40cf2cf727fb5d
k <k.r.goger@gmail.com>
2016-11-06 18:19:17 +0100

    Update configure-db.sh
    postgresql permission bug

--- configure-db.sh
+++ configure-db.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# due to an bug in postgresql
+sudo -u root gpasswd -a postgres ssl-cert
+sudo -u root chown root:ssl-cert  /etc/ssl/private/ssl-cert-snakeoil.key
+sudo -u root chmod 740 /etc/ssl/private/ssl-cert-snakeoil.key
+
+# configure
+
 sudo -u root echo "listen_addresses ='*'" >> /etc/postgresql/9.5/main/postgresql.conf
 sudo -u root echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 
